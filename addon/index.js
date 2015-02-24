c8151edd286caf2ce3df7aa5eb64266e0e32dee7
namlook <nicolas.clairon@elkorado.com>
2015-02-24 16:18:47 +0100

    make query param working \o/

--- addon/index.js
+++ addon/index.js
@@ -2,49 +2,72 @@
 import Ember from 'ember';
 import WidgetCollection from 'ember-eureka/widget-collection';
 
+
+
 export default WidgetCollection.extend({
+
+    /** add the `controllerQueryParam` as an alias of the
+     *  controller's queryParam. The name of the attribute is
+     * taken from `config.queryParam`
+     */
+    _addControllerQueryParamProperty: function() {
+        var queryParam = this.get('config.queryParam');
+        if (queryParam) {
+            this.reopen({
+                controllerQueryParam: Ember.computed.alias('currentController.'+queryParam)
+            });
+            this.set('queryParam', this.get('controllerQueryParam'));
+            this.updateQuery();
+        }
+     }.on('init'),
+
+
+    /** if the `controllerQueryParam` changed (url is modified via the back button),
+     * update the filterTerm to match the changes and update the query
+     */
+    _controllerQueryParamObserver: function() {
+        var controllerQueryParam = this.get('controllerQueryParam');
+        if (this.get('queryParam') !== controllerQueryParam) {
+            this.set('queryParam', controllerQueryParam);
+            this.updateQuery();
+        }
+    }.observes('controllerQueryParam'),
+
+
+    /** when the query has been update,
+     *  if the filterTerm doesn't match the
+     *  controllerQueryParam, update the later.
+     */
+    _routeModelQueryObserver: function() {
+        var queryParam = this.get('queryParam');
+        if (queryParam !== this.get('controllerQueryParam')) {
+            this.set('controllerQueryParam', queryParam);
+        }
+    }.observes('routeModel.query.hasChanged'),
+
+
 
     /** update the filterTerm if there is a queryParam
      */
-    filterTerm: Ember.computed.oneWay('queryParam'),
+    filterTerm: Ember.computed.alias('queryParam'),
 
     filterEnabled: Ember.computed.bool('config.filter'),
 
-    /** if there is a `queryParam` specified on the widget's config,
-     * use. Otherwise,
-     */
-     queryParam: function(key, value) {
-        var queryParam = this.get('config.filter.queryParam');
 
-        if (queryParam) {
-            key = 'currentController.'+queryParam;
-        } else {
-            key = '_queryParam';
-        }
-
-        if (arguments.length === 1) {
-            return this.get(key);
-        } else {
-            this.set(key, value);
-            return value;
-        }
-
-     }.property('config.filter.queryParam', 'currentController'),
-
-
-    updateCollection: function() {
+    updateQuery: function() {
         var filterTerm = this.get('filterTerm');
-        var query;
+        var query = this.get('routeModel.query.raw');
 
-        if (filterTerm != null) {
-            query = {'title[$iregex]': '^'+filterTerm};
+        if (filterTerm) {
+            query['title[$iregex]'] = '^'+filterTerm;
         } else {
-            query = {};
+            query['title[$iregex]'] = undefined;
+            filterTerm = null;
         }
 
-        this.set('routeModel.query', query);
-        this.set('queryParam', filterTerm);
-    }.on('init'),
+        this.set('routeModel.query.raw', query);
+    },
+
 
 
     label: function() {
@@ -69,16 +92,15 @@ export default WidgetCollection.extend({
 
     collection: function() {
         var queryConfig = this.get('queryConfig');
-        var routeQuery = this.get('routeModel.query') || {};
-        routeQuery = JSON.parse(JSON.stringify(routeQuery));
+        var routeQuery = this.get('routeModel.query.raw');
         Ember.setProperties(routeQuery, queryConfig);
         return this.get('modelStore').find(routeQuery);
-    }.property('routeModel.query', 'queryConfig'),
+    }.property('routeModel.query.hasChanged', 'queryConfig'),
 
 
     keyPress: function(e) {
         if (e.keyCode === 13) {
-            this.updateCollection();
+            this.updateQuery();
         }
     },
 
@@ -86,7 +108,7 @@ export default WidgetCollection.extend({
     actions: {
         clear: function() {
             this.set('filterTerm', null);
-            this.updateCollection();
+            this.updateQuery();
         }
     }
 
