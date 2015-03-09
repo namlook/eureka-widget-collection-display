f2fac725bcf9c3fae9d9a0252a997560dcec6486
namlook <nicolas.clairon@elkorado.com>
2015-03-09 12:54:39 +0100

    rename modelType into resource

--- addon/index.js
+++ addon/index.js
@@ -1,62 +1,30 @@
 
 import Ember from 'ember';
 import WidgetCollection from 'ember-eureka/widget-collection';
+import QueryParametrableWidgetMixin from 'eureka-mixin-query-parametrable-widget';
 
 
+export default WidgetCollection.extend(QueryParametrableWidgetMixin, {
 
-export default WidgetCollection.extend({
-
-    /** add the `controllerQueryParam` as an alias of the
-     *  controller's queryParam. The name of the attribute is
-     * taken from `config.queryParam`
+    /** set the queryParam (used for the QueryParametrableWidgetMixin),
+     * to the widget's `config.filter.queryParam`
      */
-    _addControllerQueryParamProperty: function() {
-        var queryParam = this.get('config.queryParam');
-        if (queryParam) {
-            this.reopen({
-                controllerQueryParam: Ember.computed.alias('currentController.'+queryParam)
-            });
-            this.set('queryParam', this.get('controllerQueryParam'));
-            this.updateQuery();
-        }
-     }.on('init'),
+    'config.queryParam': Ember.computed.alias('config.filter.queryParam'),
 
+    modelRoute: Ember.computed.alias('modelRoute'),
 
-    /** if the `controllerQueryParam` changed (url is modified via the back button),
-     * update the filterTerm to match the changes and update the query
-     */
-    _controllerQueryParamObserver: function() {
-        var controllerQueryParam = this.get('controllerQueryParam');
-        if (this.get('queryParam') !== controllerQueryParam) {
-            this.set('queryParam', controllerQueryParam);
-            this.updateQuery();
-        }
-    }.observes('controllerQueryParam'),
-
-
-    /** when the query has been update,
-     *  if the filterTerm doesn't match the
-     *  controllerQueryParam, update the later.
-     */
-    _routeModelQueryObserver: function() {
-        var queryParam = this.get('queryParam');
-        if (queryParam !== this.get('controllerQueryParam')) {
-            this.set('controllerQueryParam', queryParam);
-        }
-    }.observes('routeModel.query.hasChanged'),
-
-
-
-    /** update the filterTerm if there is a queryParam
-     */
+    /** Make the filterTerm a queryParam if configured in `config` */
     filterTerm: Ember.computed.alias('queryParam'),
 
+
+    /** if true, display the input filter */
     filterEnabled: Ember.computed.bool('config.filter'),
 
 
+    /** update the `routeModel.query` from `filterTerm` */
     updateQuery: function() {
         var filterTerm = this.get('filterTerm');
-        var query = this.get('routeModel.query.raw');
+        var query = this.getWithDefault('routeModel.query.raw');
 
         if (filterTerm) {
             query['title[$iregex]'] = '^'+filterTerm;
@@ -69,14 +37,13 @@ export default WidgetCollection.extend({
     },
 
 
-
     label: function() {
         var _label = this.get('config.label');
         if (_label === 'auto') {
-            return this.get('modelType');
+            return this.get('resource');
         }
         return _label;
-    }.property('config.label', 'modelType'),
+    }.property('config.label', 'resource'),
 
 
     queryConfig: function() {
@@ -90,14 +57,16 @@ export default WidgetCollection.extend({
     }.property('config.query'),
 
 
+    /** update the collection from the `routeModel.query` */
     collection: function() {
         var queryConfig = this.get('queryConfig');
         var routeQuery = this.get('routeModel.query.raw');
         Ember.setProperties(routeQuery, queryConfig);
-        return this.get('modelStore').find(routeQuery);
-    }.property('routeModel.query.hasChanged', 'queryConfig'),
+        return this.get('store').find(routeQuery);
+    }.property('routeModel.query.hasChanged', 'queryConfig', 'store'),
 
 
+    /** update the query when the user hit the enter key */
     keyPress: function(e) {
         if (e.keyCode === 13) {
             this.updateQuery();
